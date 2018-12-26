d234876c2e84f434f59ae785bea1d1a2b50aceb6
Edney <edney2507@gmail.com>
2018-12-25 23:27:41 -0200

    Alteracoesn a classe usuario

--- index.php
+++ index.php
@@ -6,9 +6,23 @@
 
 	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	//echo json_encode($usuarios);
+	//
+	//$root = new Usuario();
+	//$root->loadbyid(3);
+	//echo $root;
 
-	$root = new Usuario();
-	$root->loadbyid(3);
-	echo $root;
+	//Carrega uma lista de usuarios
+	//$lista = Usuario::getList();
+
+	//echo json_encode($lista);
+	//Carregar uma lista de usuarios buscando pelo login
+	//$search = Usuario::search("jo");
+	//echo json_encode($search);
+
+	//Teste de login
+	$usuario = new Usuario();
+	$usuario->login("root","123456");
+	echo $usuario;
+
 
 ?>
